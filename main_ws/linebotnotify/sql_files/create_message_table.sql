5c47528eed8d3712a487bd87eff36abbb6841243
bonohub13 <ken.saito.0813@gmail.com>
2021-05-28 10:43:39 +0900

    added remind_time for scheduled messages

--- main_ws/linebotnotify/sql_files/create_message_table.sql
+++ main_ws/linebotnotify/sql_files/create_message_table.sql
@@ -2,10 +2,12 @@ CONNECT linebot_user_testing/testing;
 
 -- Create table messages to store messages to notify the user with
 -- message_id is 34bit integer
+-- remind_time can store time as well (only on OracleDB)
 
 CREATE TABLE messages (
     message_id      NUMBER(10)          PRIMARY KEY,
     message_content VARCHAR2(1000 CHAR) NOT NULL,
+    remind_time     DATE
     user_id         NUMBER(5)           NOT NULL REFERENCES linebot_user(user_id)
 );
 
