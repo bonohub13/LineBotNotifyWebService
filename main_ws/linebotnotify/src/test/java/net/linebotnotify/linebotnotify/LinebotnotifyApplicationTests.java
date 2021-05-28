7290e16ed5114256d00892d3e44c9ad40a240483
bonohub13 <ken.saito.0813@gmail.com>
2021-05-28 10:15:33 +0900

    initialized project

---
+++ main_ws/linebotnotify/src/test/java/net/linebotnotify/linebotnotify/LinebotnotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.linebotnotify.linebotnotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinebotnotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
